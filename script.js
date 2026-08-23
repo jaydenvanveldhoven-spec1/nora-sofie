318089974cd7cb5ed86dca845cab06c983cf1a3d
jaydenvanveldhoven-spec1 <jaydenvanveldhoven@gmail.com>
2026-08-23 15:22:30 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function buttonOne() {
+    alert("You clicked Button 1!");
+}
+
+function buttonTwo() {
+    alert("You clicked Button 2!");
+}
+
+function buttonThree() {
+    alert("You clicked Button 3!");
+}
